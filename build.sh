42ed06b4053768368363bdb28cd740e5c8de0c23
Robert Curtis <rcurtis@arkdna.com>
2025-12-29 13:05:09 -0600

    build update

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
 set -e
 
 PROJECT_DIR="./"
 BUILD_DIR="$PROJECT_DIR/build"
 
-mkdir -p $BUILD_DIR
+mkdir -p "$BUILD_DIR"
 
+# Run all tests
 go test ./...
 
-pushd $PROJECT_DIR/cmd/server/
-  GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/gtm-linux
-popd
+# Build the Linux binary
+pushd "$PROJECT_DIR/cmd/server/" > /dev/null
+GOOS=linux GOARCH=amd64 go build -o "$BUILD_DIR/gtm-linux"
+popd > /dev/null
+
+echo "Build complete. Binaries are in $BUILD_DIR"
