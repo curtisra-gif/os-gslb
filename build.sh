73601caa548028bc9ea5443e2c0af0a1ce25b48d
Robert Curtis <rcurtis@arkdna.com>
2025-12-29 13:00:16 -0600

    build.sh update

--- build.sh
+++ build.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-PROJECT_DIR="$(go env GOPATH)/src/github.com/curtisra-gif/os-gslb"
+PROJECT_DIR="./"
 BUILD_DIR="$PROJECT_DIR/build"
 
 mkdir -p $BUILD_DIR
 
-pushd $PROJECT_DIR
-  go test ./...
-popd
-
 go test ./...
+
 pushd $PROJECT_DIR/cmd/server/
   GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/gtm-linux
-  GOOS=darwin GOARCH=amd64 go build -o $BUILD_DIR/gtm-mac
-  GOOS=windows GOARCH=amd64 go build -o $BUILD_DIR/gtm-win64
 popd
